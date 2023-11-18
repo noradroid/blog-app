ed7cde88a9616b750aa202f3c93939d81fb9a384
noradroid <75635291+noradroid@users.noreply.github.com>
2023-11-18 14:10:02 +0800

    Add swagger and openapi docs

--- src/main/java/com/example/blog/resource/CommentResource.java
+++ src/main/java/com/example/blog/resource/CommentResource.java
@@ -5,6 +5,9 @@ import com.example.blog.service.CommentService;
 import com.example.blog.service.dto.comment.CommentDto;
 import com.example.blog.service.dto.comment.CreateCommentRequestDto;
 import com.example.blog.service.dto.comment.UpdateCommentRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,11 +30,15 @@ public class CommentResource {
     @Autowired
     CommentService commentService;
 
+    @Operation(summary = "Get list of comments based on post or parent")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK")
+    })
     @GetMapping()
     public ResponseEntity<List<CommentDto>> getComments(
         @RequestParam(value = "postId", required = false) Long postId,
         @RequestParam(value = "parentId", required = false) Long parentId
-        ) {
+    ) {
         List<CommentDto> comments;
         if (parentId != null) {
             comments = commentService.getCommentsByParent(parentId);
@@ -43,6 +50,12 @@ public class CommentResource {
         return new ResponseEntity<>(comments, HttpStatus.OK);
     }
 
+    @Operation(summary = "Create comment on a post or another comment")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "Bad request"),
+        @ApiResponse(responseCode = "404", description = "Post or parent does not exist")
+    })
     @PostMapping()
     public ResponseEntity<CommentDto> createComment(
         @RequestBody CreateCommentRequestDto req
@@ -50,6 +63,12 @@ public class CommentResource {
         return new ResponseEntity<>(commentService.createComment(req), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update comment content")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "Bad request"),
+        @ApiResponse(responseCode = "404", description = "Comment does not exist")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<CommentDto> updateComment(
         @PathVariable Long id,
@@ -58,6 +77,11 @@ public class CommentResource {
         return new ResponseEntity<>(commentService.updateComment(id, req), HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete comment")
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "Deleted successfully"),
+        @ApiResponse(responseCode = "404", description = "Comment does not exist")
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteComment(@PathVariable Long id) {

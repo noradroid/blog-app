f9325f9dbae937292d388ed0d9f2093d970763eb
noradroid <75635291+noradroid@users.noreply.github.com>
2023-11-26 15:50:52 +0800

    Pass function to submit button as callback when button is clicked

--- frontend/src/app/auth/sign-up/sign-up.component.ts
+++ frontend/src/app/auth/sign-up/sign-up.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SHA256 } from 'crypto-js';
-import { switchMap } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { UserHttpService } from 'src/app/data/user/user.http.service';
 import { SubmitButtonComponent } from 'src/app/shared/form/submit-button/submit-button.component';
+import { UserRequestDto } from 'src/app/data/user/dto/user-request.model';
 
 @Component({
   selector: 'app-sign-up',
@@ -25,18 +26,17 @@ export class SignUpComponent {
     private authService: AuthService
   ) {}
 
-  submit(): void {
-    const model = {
+  submitFn: () => Observable<void> = (): Observable<void> => {
+    const model = this.convertToUserRequestDto();
+    return this.service
+      .create(model)
+      .pipe(switchMap((user) => this.authService.login(model)));
+  };
+
+  convertToUserRequestDto(): UserRequestDto {
+    return {
       username: this.model.username,
       passwordHash: SHA256(this.model.password).toString(),
     };
-    this.service
-      .create(model)
-      .pipe(
-        switchMap((user) => {
-          return this.authService.login(model);
-        })
-      )
-      .subscribe();
   }
 }
--- frontend/src/app/shared/form/submit-button/submit-button.component.ts
+++ frontend/src/app/shared/form/submit-button/submit-button.component.ts
@@ -1,18 +1,51 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+  Observable,
+  catchError,
+  combineLatestWith,
+  map,
+  of,
+  timer,
+} from 'rxjs';
+
 import { ButtonModule } from 'button';
+import { DELAY } from 'src/app/app.constants';
+import { LoaderComponent } from '../../loader/loader.component';
 
 @Component({
   selector: 'app-submit-button',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, LoaderComponent],
   templateUrl: './submit-button.component.html',
   styleUrls: ['./submit-button.component.scss'],
 })
 export class SubmitButtonComponent {
-  @Output() clickEvent = new EventEmitter<void>();
+  @Input() submitFn: () => Observable<any> = () => of();
+  @Output() completeEvent = new EventEmitter<void>();
+  loading = false;
+  error = false;
 
   handleClick(): void {
-    this.clickEvent.emit();
+    this.loading = true;
+    this.submitFn()
+      .pipe(
+        catchError((err) => of(err)),
+        combineLatestWith(timer(DELAY)),
+        map(([res, _]) => res)
+      )
+      .subscribe({
+        next: (res) => {
+          this.loading = false;
+          if (res instanceof HttpErrorResponse) {
+            this.error = true;
+          } else {
+            this.error = false;
+            this.completeEvent.emit();
+          }
+        },
+        error: (err) => {},
+      });
   }
 }

8c92497c9318c681dd8932754cc4f3c7f440c5a5
noradroid <75635291+noradroid@users.noreply.github.com>
2023-12-14 13:02:06 +0800

    Add font-size 7 for 12px

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { shareReplay } from 'rxjs';
 

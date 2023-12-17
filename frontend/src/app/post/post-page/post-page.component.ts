5fd8ed1f6faf20d52fef017f18d022c38063130a
noradroid <75635291+noradroid@users.noreply.github.com>
2023-12-18 00:09:14 +0800

    Add post page

---
+++ frontend/src/app/post/post-page/post-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-post-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post-page.component.html',
+  styleUrls: ['./post-page.component.scss']
+})
+export class PostPageComponent {
+
+}

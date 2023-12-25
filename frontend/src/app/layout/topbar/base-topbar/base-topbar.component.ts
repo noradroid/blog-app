922885c382b50b5b8517c42a23a65464a3c00376
noradroid <75635291+noradroid@users.noreply.github.com>
2023-12-25 19:54:07 +0800

    Update topbar and footer and styling

--- frontend/src/app/layout/topbar/base-topbar/base-topbar.component.ts
+++ frontend/src/app/layout/topbar/base-topbar/base-topbar.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../../logo/logo.component';
 
 @Component({
   selector: 'app-base-topbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LogoComponent],
   templateUrl: './base-topbar.component.html',
-  styleUrls: ['./base-topbar.component.scss']
+  styleUrls: ['./base-topbar.component.scss'],
 })
-export class BaseTopbarComponent {
-
-}
+export class BaseTopbarComponent {}

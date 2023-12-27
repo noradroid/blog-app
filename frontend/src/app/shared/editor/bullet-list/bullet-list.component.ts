3d667222a2d814796b2052cb63fde45604c2cfe4
noradroid <75635291+noradroid@users.noreply.github.com>
2023-12-27 22:55:05 +0800

    Make bullet list base off format option component and use radix icon

--- frontend/src/app/shared/editor/bullet-list/bullet-list.component.ts
+++ frontend/src/app/shared/editor/bullet-list/bullet-list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { radixListBullet } from '@ng-icons/radix-icons';
+import { FormatOptionComponent } from '../format-option/format-option.component';
 
 @Component({
   selector: 'app-bullet-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormatOptionComponent, NgIconComponent],
   templateUrl: './bullet-list.component.html',
   styleUrls: ['./bullet-list.component.scss'],
+  viewProviders: [provideIcons({ radixListBullet })],
 })
 export class BulletListComponent {}

5d52f15e79ebe914e1dbf2f9f9c1948291a2d690
noradroid <75635291+noradroid@users.noreply.github.com>
2023-12-30 12:42:54 +0800

    Add image domain and repository

---
+++ backend/src/main/java/com/example/blog/domain/Image.java
@@ -0,0 +1,58 @@
+package com.example.blog.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.Instant;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name = "images")
+@Data
+public class Image implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String path;
+
+    @CreatedDate
+    @Column
+    private Instant createdDate = Instant.now();
+
+    @LastModifiedDate
+    @Column
+    private Instant lastModifiedDate = Instant.now();
+
+    @Column
+    private Boolean active;
+
+//    @CreatedBy
+//    @NotNull
+//    @ManyToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @NotNull
+//    @ManyToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "post_id", nullable = false)
+//    private Post post;
+}

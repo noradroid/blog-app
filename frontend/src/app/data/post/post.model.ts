911edddcf1d5e46a6c7f5e60bbf0eb07d064ce47
noradroid <75635291+noradroid@users.noreply.github.com>
2024-01-05 23:49:01 +0800

    Add grid style and update post model with description

--- frontend/src/app/data/post/post.model.ts
+++ frontend/src/app/data/post/post.model.ts
@@ -3,6 +3,7 @@ import { User } from '../user/user.model';
 export type Post = {
   id: number;
   title: string;
+  description: string;
   content: string;
   createdDate: string;
   lastModifiedDate: string;
